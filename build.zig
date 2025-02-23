f6744a4aa8130e429dcd4f269fa679887adfa66d
Yann Bernard <yannbernard@macair.local>
2025-02-23 16:25:29 +0100

    build mod

--- build.zig
+++ build.zig
@@ -4,14 +4,40 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("homenow_protocol", .{ .root_source_file = b.path("src/homenow_protocol.zig"), .optimize = optimize, .target = target });
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+    const root_path = b.path("src/homenow_protocol.zig");
+    const name = "homenow_protocol";
+
+    _ = b.addModule(name, .{
+        .root_source_file = root_path,
     });
-    lib_unit_tests.linkLibC();
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const lib_mod = b.createModule(.{
+        .root_source_file = root_path,
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    {
+        const lib = b.addLibrary(.{
+            .name = name,
+            .root_module = lib_mod,
+            .linkage = .dynamic,
+        });
+        b.installArtifact(lib);
+        // const installed_lib = b.addInstallArtifact(lib, .{});
+        // installed_lib.emitted_h = lib.getEmittedH();
+        // b.getInstallStep().dependOn(&installed_lib.step);
+    }
+
+    { // Tests
+        const lib_unit_tests = b.addTest(.{
+            .root_module = lib_mod,
+        });
+        lib_unit_tests.linkLibC();
+
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
 }
