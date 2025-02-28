f81486c9684d1f4c7833f87aa8cb2f9b97e513ff
yann bernard <yann@arch.de>
2025-02-28 10:18:46 +0100

    deserialize

--- src/homenow_protocol.zig
+++ src/homenow_protocol.zig
@@ -9,30 +9,43 @@ pub const MessageTask = enum(u8) {
     pair = 0x03,
 };
 
-pub const MessageType = union(enum) {
-    pair: pair_message.PairMessage,
-
-    fn serialize(self: MessageType) [245]u8 {
-        switch (self) {
-            inline else => |impl| {
-                const serialized_message = impl.serialize();
-                var final_serialized_message = [_]u8{0} ** 245;
-                std.mem.copyForwards(u8, &final_serialized_message, &serialized_message);
-                return serialized_message;
-            },
-        }
-    }
-};
-
 pub const HomeNowVersion = enum(u16) {
     v0_0_0 = 0x0,
     vtest = 0xffff,
 };
 
+pub const MessageType = union(enum) {
+    pair: pair_message.PairMessage,
+
+    fn serialize(self: MessageType) [247]u8 {
+        switch (self) {
+            inline else => |impl| {
+                const serialized_message = impl.serialize();
+                var final_serialized_message = [_]u8{0} ** 247;
+                std.mem.copyForwards(u8, &final_serialized_message, &serialized_message);
+                return serialized_message;
+            },
+        }
+    }
+
+    fn deserialize(message_task: MessageTask, binary_data: [247]u8) MessageType {
+        const message_type = switch (message_task) {
+            .pair => {
+                const msg = pair_message.PairMessage.deserialize(&binary_data);
+                return MessageType{
+                    .pair = msg,
+                };
+            },
+            else => unreachable,
+        };
+        return message_type;
+    }
+};
+
 pub const HomeNowProtocol = struct {
-    version: HomeNowVersion = HomeNowVersion.v0_0_0,
-    message_task: MessageTask,
-    content: MessageType,
+    version: HomeNowVersion = HomeNowVersion.v0_0_0, // 2 B
+    message_task: MessageTask, // 1 B
+    content: MessageType, // 247 B
 
     pub fn serialize(self: HomeNowProtocol) [250]u8 {
         var content = [_]u8{0} ** 250;
@@ -50,7 +63,7 @@ pub const HomeNowProtocol = struct {
         current_mem_offset += 1;
 
         const serialized_data = self.content.serialize();
-        std.debug.assert(serialized_data.len == 245);
+        std.debug.assert(serialized_data.len == 247);
         std.mem.copyForwards(u8, content[current_mem_offset..], &serialized_data);
 
         return content;
@@ -65,43 +78,35 @@ pub const HomeNowProtocol = struct {
         const message_task: MessageTask = @enumFromInt(binary_data[current_pos]);
         current_pos += 1;
 
-        // constructing content
+        var message_content = [_]u8{0} ** 247;
+        std.mem.copyForwards(u8, &message_content, binary_data[current_pos..]);
+        const msg_content = MessageType.deserialize(message_task, message_content);
+
+        // constructing HomeNowProtocol
         const homenow_protocol = HomeNowProtocol{
             .version = version,
             .message_task = message_task,
-            .content = MessageType{
-                .pair = pair_message.PairMessage{
-                    .subtask = 1,
-                    .device_type = [_]u8{1} ** 244,
-                },
-            },
+            .content = msg_content,
         };
         return homenow_protocol;
     }
 };
 
-test "serializiation desirialization equivalence cheking" {
-    var content = [_]u8{0} ** 244;
-    var msg = [_]u8{ 10, 20, 30 };
-    std.mem.copyForwards(u8, content[0..msg.len], &msg);
-
-    const message_content = pair_message.PairMessage{
-        .subtask = 1,
-        .device_type = content,
-    };
+test "single serialization and deserialization equalization of HomeNowProtocol" {
+    const msg = [_]u8{ 10, 20, 30 };
+    const sub_task = 1;
+    const message_content = pair_message.PairMessage.init(sub_task, &msg);
     const home = HomeNowProtocol{
         .version = HomeNowVersion.vtest,
         .message_task = MessageTask.pair,
-        .content = MessageType{
-            .pair = message_content,
-        },
+        .content = MessageType{ .pair = message_content },
     };
 
-    const serialized_data = home.serialize();
-    std.debug.print("Serialized data: {any}\nWith length: {d}\n\n", .{ serialized_data, serialized_data.len });
-    try expect(serialized_data.len == 250);
+    const home_bytes = home.serialize();
+    try expect(home_bytes.len == 250);
 
-    const homenow_d = HomeNowProtocol.deserialize(serialized_data);
-    std.debug.print("Deserialized: {any}\n", .{homenow_d});
-    std.debug.print("With content: {any}\n", .{homenow_d.content.pair.device_type});
+    const homenow_protocol = HomeNowProtocol.deserialize(home_bytes);
+
+    const equal = std.meta.eql(homenow_protocol, home);
+    try expect(equal);
 }
--- src/messages/pair_message.zig
+++ src/messages/pair_message.zig
@@ -1,14 +1,47 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub const PairMessage = struct {
-    subtask: u8,
-    device_type: [244]u8,
+    subtask: u8, // 1 B
+    device_type: [246]u8, // 246 B
 
-    pub fn serialize(self: PairMessage) [245]u8 {
-        var content = [_]u8{0} ** 245;
+    pub fn init(subtask: u8, device_type: []const u8) PairMessage {
+        std.debug.assert(device_type.len <= 246);
+        var content = [_]u8{0} ** 246;
+        std.mem.copyForwards(u8, &content, device_type);
+        return .{
+            .subtask = subtask,
+            .device_type = content,
+        };
+    }
+
+    pub fn serialize(self: PairMessage) [247]u8 {
+        var content = [_]u8{0} ** 247;
         content[0] = self.subtask;
         std.mem.copyForwards(u8, content[1..], &self.device_type);
 
         return content;
     }
+
+    pub fn deserialize(content: []const u8) PairMessage {
+        std.debug.assert(content.len >= 2 and content.len <= 247);
+        const subtask = content[0];
+        var device_type = [_]u8{0} ** 246;
+        std.mem.copyForwards(u8, &device_type, content[1..]);
+
+        return PairMessage{
+            .subtask = subtask,
+            .device_type = device_type,
+        };
+    }
 };
+
+test "single serialization and deserialization equalization of PairMessage" {
+    const sub_task = 3;
+    const device_type = [_]u8{ 2, 3, 5, 10, 19, 1 };
+    const pair_message = PairMessage.init(sub_task, &device_type);
+
+    const pair_message_ser = pair_message.serialize();
+    const pair_message_des = PairMessage.deserialize(&pair_message_ser);
+    try expect(std.meta.eql(pair_message, pair_message_des));
+}
